33d2e6623778a09e748f8d673f09b953690c903d
Jacob Bryan <jtb36688@gmail.com>
2019-05-17 00:41:46 -0400

    finished MVP with all tests passing

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -9,7 +9,6 @@ class Heap:
 
   def delete(self):
     deleted = self.storage.pop(0)
-    print("deleted value", deleted)
     if len(self.storage) > 1:
       temp = self.storage[-1]
       self.storage[-1] = self.storage[0]
@@ -36,53 +35,20 @@ class Heap:
 
   def _sift_down(self):
     end = len(self.storage) - 1
-    print("sift down runs", self.storage)
-    for i in self.storage:
-      index = i-1
+    for i in range(len(self.storage)):
+      index = i
       leftchildindex = 2*index + 1
       rightchildindex = leftchildindex + 1
-      if index == 3:
-        print("2 running with left index of", leftchildindex)
       if leftchildindex <= end:
         prioritychild = leftchildindex
-        if self.storage[index] == 5:
-          print("5 is being check against", self.storage[prioritychild])
         if rightchildindex <= end and self.storage[rightchildindex] > self.storage[prioritychild]:
-          print("right was greater", self.storage[rightchildindex])
           prioritychild = rightchildindex
         if self.storage[prioritychild] > self.storage[index]:
-            print("swap made between", self.storage[prioritychild], self.storage[index])
             temp = self.storage[prioritychild]
             self.storage[prioritychild] = self.storage[index]
             self.storage[index] = temp
             return self._sift_down()
-      else:
-        if index <= end and self.storage[index] == 5:
-          print("check failed for 5 with lci of", leftchildindex)
-            
-
-
-    # if leftchildindex <= end:
-    #   prioritychild = leftchildindex
-    #   while index <= end:
-    #     print(self.storage[prioritychild])
-    #     if rightchildindex <= end and self.storage[rightchildindex] > self.storage[prioritychild]:
-    #       prioritychild = rightchildindex
-    #       print("right child used", self.storage[prioritychild])
-    #     if self.storage[prioritychild] > self.storage[index]:
-    #       temp = self.storage[prioritychild]
-    #       self.storage[prioritychild] = self.storage[index]
-    #       self.storage[index] = temp
-    #       index = prioritychild
-    #       leftchildindex = 2*index + 1
-    #       rightchildindex = leftchildindex + 1
-    #     else:
-    #       break
-
-
-        
-       
-
+      
     # check both left and right are defined, and compare to parent. 
     # assure parent is greater than children
     # if both are larger, swap with whichever child is larger
